904485792e629625614812dc712a79d81de17d49
dhengghuring <wyvrendragon@gmail.com>
2023-02-10 00:48:20 +0700

    Update end poit and try fetch

--- authentication/backend/controllers/Users.js
+++ authentication/backend/controllers/Users.js
@@ -1,10 +1,45 @@
 const User = require("../models/UserModel");
+const { argon2 } = "argon2";
+
 module.exports = {
-  getUsers: (req, res) => {},
+  getUsers: async (req, res) => {
+    try {
+      const response = await User.findAll();
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({ msg: error.message });
+    }
+  },
 
-  getUserById: (req, res) => {},
+  getUserById: async (req, res) => {
+    try {
+      const response = await User.findOne({ where: { uuid: req.params.id } });
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({ msg: error.message });
+    }
+  },
 
-  createUser: (req, res) => {},
+  createUser: async (req, res) => {
+    const { name, email, password, confPassword, role } = req.body;
+    console.log("yok gas!!", name, email, password, confPassword, role);
+    if (password !== confPassword)
+      return res
+        .status(400)
+        .json({ msg: "Password dan confirm password tidak cocok" });
+    const hashPassword = await argon2.hash(password);
+    try {
+      await User.create({
+        name: name,
+        email: email,
+        password: hashPassword,
+        role: role,
+      });
+      res.status(201).json({ msg: "Register Akun Berhasil" });
+    } catch (error) {
+      res.status(400).json({ msg: error.message });
+    }
+  },
 
   updateUser: (req, res) => {},
 
--- authentication/backend/index.js
+++ authentication/backend/index.js
@@ -14,38 +14,38 @@ const db = require("./config/Database");
 dotenv.config();
 
 const app = express();
-
-//** Mengkoneksikan ke database */
-(async () => {
-  await db.sync();
-})();
+// //** Mengkoneksikan ke database */
+// (async () => {
+//   await db.sync();
+// })();
 
 //** Membuat middleware */
-app.use(
-  session({
-    secret: process.env.SESS_SECREAT,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      // secure cookienya dibuat 'auto' supaya secara otomatis terset ketika alamatnya menggunakan http atau https
-      secure: "auto",
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESS_SECREAT,
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//       // secure cookienya dibuat 'auto' supaya secara otomatis terset ketika alamatnya menggunakan http atau https
+//       secure: "auto",
+//     },
+//   })
+// );
 
 app.use(
   cors({
     // cors credential berniali ture berfungsi agar frontend bisa mengirim request dan juga cookie beserta credentialnya
-    credentials: true,
+    credentials: false,
     // origin wadah untuk mengizinkan domain apa saja yang bisa mengakses api yang sudah dibuat bisa berupa array jika ada banyak domain
-    origin: "http://localhost:4000/",
+    origin: "http://localhost:" + process.env.APP_PORT,
   })
 );
 // middleware yang berfungsi agar backend bisa menerima data beruapa json dari fe
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(UserRoute);
 app.use(BookRoute);
 //** Menghubungkan Port yang sudah di set di .env */
 app.listen(process.env.APP_PORT, () => {
-  console.log("Server berjalan...");
+  console.log("Server berjalan... di PORT " + process.env.APP_PORT);
 });
